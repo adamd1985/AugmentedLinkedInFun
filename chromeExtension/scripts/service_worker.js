bc79639769ce7388a19f504a2c703ecdb89bee65
adamd1985 <63671735+adamd1985@users.noreply.github.com>
2023-05-17 00:05:12 +0200

    Code ready

--- chromeExtension/scripts/service_worker.js
+++ chromeExtension/scripts/service_worker.js
@@ -9,10 +9,9 @@
  */
 import * as use from '@tensorflow-models/universal-sentence-encoder';
 import * as tf from '@tensorflow/tfjs';
-import SAVED_MODEL from '../../notebooks/models/tfjs/model.json'
-import { error } from 'jquery';
+import * as tfc from '@tensorflow/tfjs-converter'
 
-const DEFAULT_MODEL_PATH='./model.json'
+const DEFAULT_MODEL_PATH = './model.json'
 
 /**
  * Async loads a USE and our Custom models on construction.  
@@ -24,49 +23,42 @@ class ProfileClassifier {
      * Constructor.
      * @param {*} savedClassifier The classifier to load from. Can be url string or the json file.
      */
-    constructor(savedClassifier=null) {
-      this.savedClassifier =  savedClassifier;
+    constructor(savedClassifier = null) {
+        this.savedClassifier = savedClassifier;
     }
-  
+
     /**
      * Loads USE and custom Models, warms the models up.
      */
     async loadModel() {
-      console.log('Loading models...');
-      if (!this.savedClassifier){
-        console.error('No model.json to load from!');
-        throw new error("No model.json to load from!")
-      }
-      try {
-        // Chrome will have a different path setup for extensions.
-        console.log(`Loading USE model...`)
-        this.useModel = await use.load();
+        console.log('Loading models...');
+        if (!this.savedClassifier) {
+            console.error('No model.json to load from!');
+            throw new Error("No model.json to load from!")
+        }
+        try {
+            // Chrome will have a different path setup for extensions.
+            console.log(`Loading USE model...`)
+            this.useModel = await use.load();
 
-        console.log(`Loading custom model...`)
-        this.model = await tf.loadGraphModel(this.savedClassifier);
+            console.log(`Loading custom model...`)
+            this.model = await tfc.loadGraphModel(this.savedClassifier);
 
-        tf.tidy(async () => {
             // Warms up the models by causing intermediate tensor values to be built.
             const startTime = performance.now();
-            this.useModel.embed(["Test descriptions"])
-                .then((embeddings) => {
-                    console.log(`Successfully embeded test text: ${embeddings}. Running prediction.`);
-                    return this.model.predict(embeddings)
-                })
-                .then((result) => {
-                    console.log(`Model loaded with ${result}.`);   
-                })
-                .catch((e) => console.error(`Error predicting models: ${e}`))
-                .finally(() => {
-                    const totalTime = Math.floor(performance.now() - startTime);
-                    console.log(`Models initialized in ${totalTime} ms...`);
-                })
-        });
-      } catch (e) {
-        console.error('Unable to load models', e);
-      }
+            const embeddings = await this.useModel.embed(["Test descriptions"])
+            const result = await this.model.predict(embeddings);
+
+            console.log(`Model loaded with ${result}.`);
+
+            const totalTime = Math.floor(performance.now() - startTime);
+            console.log(`Models initialized in ${totalTime} ms...`);
+
+        } catch (e) {
+            console.error('Unable to load models', e);
+        }
     }
-  
+
     /**
      * Triggers our models to make a prediction.
      * @param {descriptions} string Profile title and descriptions.
@@ -80,23 +72,25 @@ class ProfileClassifier {
         if (!this.model || !this.useModel) {
             console.log('Waiting for models to load...');
             setTimeout(
-            () => { this.classifyProfile(descriptions, sendResponse) }, 2000);
+                () => { this.classifyProfile(descriptions, sendResponse) }, 2000);
             return;
         }
-        console.log('Predicting...');
+        console.log(`Predicting for ${descriptions}`);
         const startTime = performance.now();
 
-        const embeddings = await this.useModel.embed([message.titles]);
+        const embeddings = await this.useModel.embed([descriptions]);
+        console.log(`embeddings: ${embeddings}`);
         const t = await this.model.predict(embeddings);
         const predictions = t.dataSync();
+        console.log(`predictions: ${predictions}`);
 
         const totalTime = performance.now() - startTime;
         console.log(`Done in ${totalTime.toFixed(1)} ms `);
 
         const LABELS = ["bigbird", "count", "grover", "grouch", "erniebert"];
-        const idx = predictions.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);   
+        const idx = predictions.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
 
-        sendResponse({ 'proba': predictions[idx], 'label': (LABELS[idx] ?? "N/A")});
+        sendResponse({ 'proba': Math.floor(predictions[idx] * 100), 'label': (LABELS[idx] ?? "N/A") });
     }
 }
 
@@ -129,49 +123,43 @@ class ProfileScrapper {
      * @param {descriptions} string Profile title and descriptions.
      * @param {sendResponse} function messaging callback to reply to the content script.
      */
-    async scrapeProfile(descriptions, sendResponse) {
+    async scrapeProfile(link, sendResponse) {
         chrome.tabs
-        .create({ url: message.link, active: false })
-        .then(tab => {
-            let tabID = tab.id
-            chrome.scripting.executeScript({
-                func: getProfile,
-                target : {
-                    tabId: tabID
-                },
-                injectImmediately: false
+            .create({ url: link, active: false })
+            .then(tab => {
+                let tabID = tab.id
+                chrome.scripting.executeScript({
+                    func: getProfile,
+                    target: {
+                        tabId: tabID
+                    },
+                    injectImmediately: false
+                }).then(injectionResults => {
+                    // Should have 1 parent frame.
+                    let profile = injectionResults[0]?.result;
+                    console.log(`injectionResults: ${profile}`)
+                    return sendResponse({ profile: profile });
+                })
+                    .then(() => chrome.tabs.remove(tabID))
+                    .catch((error) => {
+                        console.error(error.message);
+                        sendResponse("BAD");
+                    })
             })
-            .then(injectionResults => {
-                // Should have 1 parent frame.
-                let profile = injectionResults[0]?.result;
-                console.log(`injectionResults: ${profile}`)
-                return sendResponse({ profile: profile});
-            })
-            .then(() => chrome.tabs.remove(tabID))
-            .catch((error) => {
-                console.error(error.message);
-                sendResponse("BAD");
-            })
-        })
     }
 }
 
 // Global variables.
-let PROFILE_CLASSIFIER = null;
-let PROFILE_SCRAPPER = null;
+const PROFILE_CLASSIFIER = new ProfileClassifier("https://raw.githubusercontent.com/adamd1985/AugmentedLinkedInFun/master/notebooks/models/tfjs/model.json");
+const PROFILE_SCRAPPER = new ProfileScrapper();
+PROFILE_CLASSIFIER.loadModel();
 
 /**
  * Extension load.
  * 
- * Will initialize everything here.
  */
 chrome.runtime.onInstalled.addListener(async function () {
     console.log("CoPilot loaded, models loading and initializing.");
-
-    PROFILE_CLASSIFIER = new ProfileClassifier("https://raw.githubusercontent.com/adamd1985/AugmentedLinkedInFun/master/notebooks/models/tfjs/model.json");
-    PROFILE_SCRAPPER = new  ProfileScrapper();
-
-    PROFILE_CLASSIFIER.loadModel();
 });
 
 /**
@@ -181,7 +169,7 @@ chrome.commands.onCommand.addListener((command) => {
     console.log(`Command: ${command}, sending message to scrape.`);
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         // Get the current TAB.
-        chrome.tabs.sendMessage(tabs[0].id,{ cmd: "run" });  
+        chrome.tabs.sendMessage(tabs[0].id, { cmd: "run" });
     });
 });
 
@@ -190,6 +178,10 @@ chrome.commands.onCommand.addListener((command) => {
  * If we return TRUE, we are informing chrome that the result of the call will happen asynch.
  */
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (!PROFILE_SCRAPPER || !PROFILE_CLASSIFIER) {
+        throw new Error("Classes are null!");
+    }
+
     console.log(`Message from ${sender}: ${JSON.stringify(message)}`)
     if (message?.link) {
         PROFILE_SCRAPPER.scrapeProfile(message.link, sendResponse);
@@ -197,7 +189,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     else if (message?.descriptions) {
         PROFILE_CLASSIFIER.classifyProfile(message.descriptions, sendResponse);
     }
-    else{
+    else {
         console.warn(`An unknown message ${message} was received.`);
         return false;
     }
@@ -207,6 +199,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 /**
  * Unload
  */
-chrome.runtime.onSuspend.addListener(function() {
+chrome.runtime.onSuspend.addListener(function () {
     console.log("Unloading.");
 });
