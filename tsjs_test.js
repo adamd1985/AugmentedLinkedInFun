09f416ba821c7461fb8b5c1ad24d1394b25caaf5
adamd1985 <63671735+adamd1985@users.noreply.github.com>
2023-05-16 13:51:08 +0200

    Cleaned

--- tsjs_test.js
+++ tsjs_test.js
@@ -1,16 +1,20 @@
 const tf = require("@tensorflow/tfjs");
-//const tfn = require("@tensorflow/tfjs-node");
+const tfn = require("@tensorflow/tfjs-node");
 const use = require('@tensorflow-models/universal-sentence-encoder');
+const tfc = require('@tensorflow/tfjs-converter');
 
-//const handler = tfn.io.fileSystem('./notebooks/models/tfjs/model.json');
+const handler = tfn.io.fileSystem('./notebooks/models/tfjs/model.json');
 
 async function testModel(){
     const url = "https://raw.githubusercontent.com/adamd1985/AugmentedLinkedInFun/master/notebooks/models/tfjs/model.json";
 
-    const model = await tf.loadGraphModel("file://C:/Users/bigworker/workspace/NLPLinkedIn/notebooks/models/tfjs/model.json");
+    const model = await tfc.loadGraphModel(url);
 
     const useModel = await use.load();
-    const embeddings = await useModel.embed(["IT Consultant at Sesame Street, lord of Java Code, who likes to learn new stuff and tries some machine learning in my free engineering time."])
+    
+    let arr = ["IT Consultant at Sesame Street, lord of Java Code, who likes to learn new stuff and tries some machine learning in my free engineering time."];
+
+    const embeddings = await useModel.embed(arr);
 
     const t = await model.predict(embeddings);
 
